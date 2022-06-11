4d243e77849a55674e41e4ed61ce4c85f2e035b1
Muzaffer Atmaca <muzafferatmaca@gmail.com>
2022-06-11 19:55:37 +0300

    Initial commit

---
+++ app/src/main/java/com/muzafferatmaca/notesapp/MainFragment.kt
@@ -0,0 +1,36 @@
+package com.muzafferatmaca.notesapp
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+
+
+class MainFragment : Fragment() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+
+
+        val view = LayoutInflater.from(context).inflate(R.layout.fragment_main, container, false)
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+
+}
