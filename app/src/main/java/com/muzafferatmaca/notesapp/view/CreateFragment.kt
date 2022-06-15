387b74f80e234b83d1684c1614b2ad410d8da892
Muzaffer Atmaca <muzafferatmaca@gmail.com>
2022-06-15 14:18:33 +0300

    update

--- app/src/main/java/com/muzafferatmaca/notesapp/view/CreateFragment.kt
+++ app/src/main/java/com/muzafferatmaca/notesapp/view/CreateFragment.kt
@@ -1,5 +1,9 @@
 package com.muzafferatmaca.notesapp.view
 
+import android.content.BroadcastReceiver
+import android.content.Context
+import android.content.Intent
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -12,6 +16,7 @@ import androidx.navigation.Navigation
 import com.muzafferatmaca.notesapp.R
 import com.muzafferatmaca.notesapp.databinding.FragmentCreateBinding
 import com.muzafferatmaca.notesapp.model.Notes
+import com.muzafferatmaca.notesapp.util.NoteBottomSheetFragment
 import com.muzafferatmaca.notesapp.viewmodel.CreateFragmentViewModel
 import kotlinx.android.synthetic.main.fragment_create.*
 import java.text.SimpleDateFormat
@@ -22,7 +27,7 @@ class CreateFragment : Fragment() {
 
     private lateinit var viewModel: CreateFragmentViewModel
     private val currentDate = SimpleDateFormat.getDateInstance().format(Date())
-    var selectedColor = "#171C26"
+    var selectedColor = "#191A2C"
     private var webLink = ""
     private var selectedImagePath = ""
 
@@ -49,13 +54,10 @@ class CreateFragment : Fragment() {
 
 
         dateTimeTextView.text = currentDate
+        setDoneImageView()
+        setBackImageView()
+        setMoreImageView()
 
-        doneImageView.setOnClickListener {
-
-            val action = CreateFragmentDirections.actionCreateFragmentToMainFragment()
-            Navigation.findNavController(view).navigate(action)
-            saveNote()
-        }
     }
 
     private fun saveNote() {
@@ -85,6 +87,115 @@ class CreateFragment : Fragment() {
                     selectedColor
                 )
             )
+        }
+
+    }
+
+    private fun setDoneImageView() {
+
+        doneImageView.setOnClickListener {
+
+            val action = CreateFragmentDirections.actionCreateFragmentToMainFragment()
+            Navigation.findNavController(it).navigate(action)
+            saveNote()
+        }
+
+    }
+
+    private fun setBackImageView() {
+
+        backImageView.setOnClickListener {
+
+            val action = CreateFragmentDirections.actionCreateFragmentToMainFragment()
+            Navigation.findNavController(it).navigate(action)
+
+        }
+
+    }
+
+    private fun setMoreImageView() {
+
+        var noteBottomSheetFragment = NoteBottomSheetFragment.newInstance()
+        noteBottomSheetFragment.show(
+            requireActivity().supportFragmentManager,
+            "Note Bottom Sheet Fragment"
+        )
+
+    }
+
+    private val BroadcastReceiver: BroadcastReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context?, intent: Intent?) {
+
+            var actionColor = intent!!.getStringExtra("action")
+
+            when (actionColor!!) {
+
+
+                "Blue" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+
+                "Yellow" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+
+
+                "Purple" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+
+
+                "Green" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+
+
+                "Orange" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+
+
+                "Black" -> {
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+
+                "Image" ->{
+                    readStorageTask()
+                    layoutWebUrl.visibility = View.GONE
+                }
+
+                "WebUrl" ->{
+                    layoutWebUrl.visibility = View.VISIBLE
+                }
+                "DeleteNote" -> {
+                    //delete note
+                    deleteNote()
+                }
+
+
+                else -> {
+                    layoutImage.visibility = View.GONE
+                    imgNote.visibility = View.GONE
+                    layoutWebUrl.visibility = View.GONE
+                    selectedColor = p1.getStringExtra("selectedColor")!!
+                    colorView.setBackgroundColor(Color.parseColor(selectedColor))
+
+                }
+
+            }
+
         }
 
     }
