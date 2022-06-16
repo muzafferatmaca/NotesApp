8aec3431ba5791d063071b9cdc12e81a4d2560b2
Muzaffer Atmaca <muzafferatmaca@gmail.com>
2022-06-16 22:53:38 +0300

    add permission

--- app/src/main/java/com/muzafferatmaca/notesapp/view/CreateFragment.kt
+++ app/src/main/java/com/muzafferatmaca/notesapp/view/CreateFragment.kt
@@ -1,11 +1,16 @@
 package com.muzafferatmaca.notesapp.view
 
+import android.Manifest
+import android.app.Activity
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
 import android.content.IntentFilter
+import android.graphics.BitmapFactory
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -190,6 +195,18 @@ class CreateFragment : Fragment(), EasyPermissions.PermissionCallbacks,EasyPermi
 
                 }
 
+                "Image" ->{
+                    readStorageTask()
+                    layoutWebUrl.visibility = View.GONE
+                }
+
+                "WebUrl" ->{
+                    layoutWebUrl.visibility = View.VISIBLE
+                }
+                "DeleteNote" -> {
+                    //delete note
+                }
+
             }
 
         }
@@ -203,6 +220,77 @@ class CreateFragment : Fragment(), EasyPermissions.PermissionCallbacks,EasyPermi
 
     }
 
+    private fun hasReadStoragePerm():Boolean{
+        return EasyPermissions.hasPermissions(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE)
+    }
+
+
+    private fun readStorageTask(){
+        if (hasReadStoragePerm()){
+
+
+            pickImageFromGallery()
+        }else{
+            EasyPermissions.requestPermissions(requireActivity(), getString(R.string.storage_permission_text),
+                READ_STORAGE_PERM, Manifest.permission.READ_EXTERNAL_STORAGE
+            )
+        }
+    }
+
+    private fun pickImageFromGallery(){
+        var intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        if (intent.resolveActivity(requireActivity().packageManager) != null){
+            startActivityForResult(intent,REQUEST_CODE_IMAGE)
+        }
+    }
+
+
+    private fun getPathFromUri(contentUri: Uri): String? {
+        var filePath:String? = null
+        var cursor = requireActivity().contentResolver.query(contentUri,null,null,null,null)
+        if (cursor == null){
+            filePath = contentUri.path
+        }else{
+            cursor.moveToFirst()
+            var index = cursor.getColumnIndex("_data")
+            filePath = cursor.getString(index)
+            cursor.close()
+        }
+        return filePath
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE_IMAGE && resultCode == Activity.RESULT_OK){
+            if (data != null){
+                var selectedImageUrl = data.data
+                if (selectedImageUrl != null){
+                    try {
+                        var inputStream = requireActivity().contentResolver.openInputStream(selectedImageUrl)
+                        var bitmap = BitmapFactory.decodeStream(inputStream)
+                        imgNote.setImageBitmap(bitmap)
+                        imgNote.visibility = View.VISIBLE
+                        layoutImage.visibility = View.VISIBLE
+
+                        selectedImagePath = getPathFromUri(selectedImageUrl)!!
+                    }catch (e:Exception){
+                        Toast.makeText(requireContext(),e.message,Toast.LENGTH_SHORT).show()
+                    }
+
+                }
+            }
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        EasyPermissions.onRequestPermissionsResult(requestCode,permissions,grantResults,requireActivity())
+    }
     override fun onPermissionsGranted(requestCode: Int, perms: MutableList<String>) {
         TODO("Not yet implemented")
     }
